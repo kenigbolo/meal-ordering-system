c1c24cf60b25bc3c0d975cdfe3a83c53c029be6d
kenigbolo <kenigbol@ut.ee>
2016-08-17 21:33:55 +0300

    Modify the sessions controller to finish the implementation of Facebook login using Omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.from_omniauth(request.env["omniauth.auth"])
-		session[:user_id] = user.user_id
+		session[:user_id] = user.id
 		flash[:notice] = "Signed in"
 		redirect_to root_path
 	end
 
 	def destroy
-		session.delete(user_id) if current_user
+		session.delete(:user_id) if current_user
 		redirect_to root_path
 	end
 end
