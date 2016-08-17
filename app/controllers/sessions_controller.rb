5a3fd2d49b1e9b0edccbe82c407c7eca8446f575
kenigbolo <kenigbol@ut.ee>
2016-08-17 21:12:07 +0300

    Create sessions controller to handle signup and login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+	def create
+		user = User.from_omniauth(request.env["omniauth.auth"])
+		session[:user_id] = user.user_id
+		flash[:notice] = "Signed in"
+		redirect_to root_path
+	end
+
+	def destroy
+		session.delete(user_id) if current_user
+		redirect_to root_path
+	end
+end
