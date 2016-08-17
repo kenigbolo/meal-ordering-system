b94a7b1fe0b040b865711a0fde0019ba5232dc79
kenigbolo <kenigbol@ut.ee>
2016-08-17 21:43:27 +0300

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	user = User.new
+	before(:all) do
+		user.provider = "facebook"
+		user.user_code = "1355889534440499"
+		user.name = "Kenigbolo Meya Stephen"
+  		user.email = "expensivestephen@gmail.com"
+  	end
+
+  	describe ".from_omniauth" do
+  		it {is_expected.to be_valid}
+  	end
+
+  	describe ".from_omniauth" do
+  		it "saves new user" do
+  			expect(User.from_omniauth(set_valid_omniauth)).to be_a User
+  		end
+  	end
+  
 end
