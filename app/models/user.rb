49a17059f40a63954648930a7a9c51564d84f208
kenigbolo <kenigbol@ut.ee>
2016-08-17 21:20:11 +0300

    Modify user model to handle creation of user from oAuth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, user_code: auth.uid).first_or_create do |user|
+			user.provider = auth.provider
+			user.user_code = auth.uid
+			user.email = auth.info.email
+			user.name = auth.info.name
+			user.save
+		end
+	end
 end
