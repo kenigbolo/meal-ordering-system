591fb75166e8268bd6b2793284818337a2365e42
kenigbolo <kenigbol@ut.ee>
2016-08-17 20:19:54 +0300

    Create user model and user spec

---
+++ db/migrate/20160817171912_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :provider
+      t.string :user_code
+
+      t.timestamps
+    end
+  end
+end
