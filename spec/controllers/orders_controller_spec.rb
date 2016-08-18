629aaeade64162bd5bc2f9e2b516b53caa2958e2
kenigbolo <kenigbol@ut.ee>
2016-08-18 13:48:42 +0300

    Add status code test to index method test

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe OrdersController, type: :controller do
 	it "renders the index template" do
       get :index
       expect(response).to render_template("index")
+      expect(response.status).to eq(200)
     end
 end
