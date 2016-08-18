1dc344e2a3bf03dc59ce0c611e8c03037035b768
kenigbolo <kenigbol@ut.ee>
2016-08-18 13:45:32 +0300

    Add test for index controller

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-# RSpec.describe OrdersController, type: :controller do
+RSpec.describe OrdersController, type: :controller do
 
-# end
+	it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+end
