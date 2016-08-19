d4a2ab480f943b4b84bb015a21d9168346c61146
kenigbolo <kenigbol@ut.ee>
2016-08-19 23:21:17 +0300

    Add unit tests for meal model

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
+meal = Meal.new(user_id: "1")
+	it "is not valid without a User ID" do
+	  meal = Meal.new
+	  expect(meal).to_not be_valid
+	end
 
+	it "is not valid without a creation date" do
+	  meal.created_at = nil
+	  meal.save
+	  expect(meal).to_not be_valid
+	end
 
+	it "is not valid without a date for updated_at" do
+	  meal.updated_at = nil
+	  meal.save
+	  expect(meal).to_not be_valid
+	end
 end
