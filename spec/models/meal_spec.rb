f35e89ecf5834fd06ed4524261f2a9f1ddb0e1a5
kenigbolo <kenigbol@ut.ee>
2016-08-19 11:38:15 +0300

    Modify some tests

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -2,9 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
 
- #  	test "should not save meal without user_id" do
-	#   meal = Meal.new
-	#   assert_not meal.save
-	# end
 
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  	
- #  	test "should create new Order" do
-	#   order = Order.new
-	#   assert order.save
-	# end
+  	order = Order.new
+	before(:all) do
+		order.restaurant = "restaurant"
+		order.meal_order = []
+		order.status = "Active"
+  	end
+
+	  	describe "#create" do
+		  	it "should create new Order" do
+			  assert order.save
+			end
+		end
 end
